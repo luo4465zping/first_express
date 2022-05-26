94498c47bfdbca6789316619dc903be0229555e9
alalal <857580334@qq.com>
2022-05-26 15:49:36 +0800

    注册功能的实现

--- router/hander_router.js
+++ router/hander_router.js
@@ -1,8 +1,23 @@
+const db=require("../db/index")
 exports.myregister=function (req,res) {
     const userinfo=req.body
+    // 查询输入是否合法
     if(!userinfo.username ||!userinfo.password){
         return res.send({status:1,msg:"请检查用户名或密码是否合法"})
     }
+// 查询用户是否已经注册
+    const sqlstr="select * from ev_user where username=?"
+    db.query(sqlstr,[userinfo.username],(err,result)=>{
+        // 执行sql失败
+        if(err){
+            return res.send({status:1,msg:err.msg})
+        }
+        // 判断用户是否占用
+        if(result.length>0){
+            return res.send({status:1,msg:"请更换用户名或密码是否合法"})
+        }
+
+    })
     console.log("正在注册请求");
     res.send(userinfo)
 }

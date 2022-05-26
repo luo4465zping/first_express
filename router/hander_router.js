eef7bf84dcd5b8f3e78e6e7995692a03281e99ee
alalal <857580334@qq.com>
2022-05-26 14:33:46 +0800

    数据库的配置

--- router/hander_router.js
+++ router/hander_router.js
@@ -1,6 +1,10 @@
 exports.myregister=function (req,res) {
+    const userinfo=req.body
+    if(!userinfo.username ||!userinfo.password){
+        return res.send({status:1,msg:"请检查用户名或密码是否合法"})
+    }
     console.log("正在注册请求");
-    res.send("register ok")
+    res.send(userinfo)
 }
 exports.mylogin=function (req,res) {
     console.log("正在登录请求");

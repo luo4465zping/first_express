96d5e29841914c06437d1e253e540ff6b4b52b90
alalal <857580334@qq.com>
2022-05-26 15:02:39 +0800

    啊啊啊，数据库更换了

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 const db=mysql.createPool({
     host: '10.1.222.244', // 服务器地址
     user: 'zcajbi', // mysql用户名称
-    password: 'zcaj@2021', // mysql用户密码
+    password: '*****', // mysql用户密码
     port: '3306', // 端口
     database: '各类报表', // 数据库名称
 })

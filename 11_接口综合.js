e716523be62c706e165c4b6426746ce6eb132bd4
857580334@qq.com <857580334@qq.com>
2022-05-27 11:27:39 +0800

    优化代码

--- 11_接口综合.js
+++ 11_接口综合.js
@@ -6,6 +6,16 @@ const cors=require("cors")
 app.use(cors())
 //配置解析表单数据中间件
 app.use(express.urlencoded({extended:false}))
+// 使用中间件优化代码，
+app.use(function (req,res,next) {
+    res.cc=function (err,status=1) {
+        res.send({
+            status,
+            msg:err instanceof Error ? err.message:err,
+        })
+    }
+  next()  
+})
 app.use("/api",routers)
 app.listen(80,()=>{
     console.log("服务器启动成功");

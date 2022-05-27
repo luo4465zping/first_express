634945d3018872530da3658d9690c4a28ffdf862
857580334@qq.com <857580334@qq.com>
2022-05-27 11:26:56 +0800

    用户注册完成

--- router/hander_router.js
+++ router/hander_router.js
@@ -1,4 +1,5 @@
 const db=require("../db/index")
+const bcrypt=require("bcrypt")
 exports.myregister=function (req,res) {
     const userinfo=req.body
     // 查询输入是否合法
@@ -10,16 +11,32 @@ exports.myregister=function (req,res) {
     db.query(sqlstr,[userinfo.username],(err,result)=>{
         // 执行sql失败
         if(err){
-            return res.send({status:1,msg:err.msg})
+            // return res.send({status:1,msg:err.msg})
+            return res.cc(err)
         }
         // 判断用户是否占用
         if(result.length>0){
-            return res.send({status:1,msg:"请更换用户名或密码是否合法"})
+            // return res.send({status:1,msg:"请更换用户名或密码是否合法"})
+            return res.cc("请更换用户名或密码是否合法")
         }
+        userinfo.password=bcrypt.hashSync(userinfo.password,10)
+        // 进行插值操作
+        const sql="insert into ev_user set ?"
+        db.query(sql,{username:userinfo.username,password:userinfo.password},(errs,results)=>{
+            if(err){
+                // return res.send({status:1,msg:err.msg})
+                return res.cc(err)
+            }
+            if(results.affectedRows !=1){
+                // return res.send({status:1,msg:"创建用户失败，请稍后再试"})
+                return res.cc("创建用户失败，请稍后再试")
+            }
+            //  res.send({status:0,msg:"创建用户成功"})
+            return res.cc("创建用户成功",0)
+        })
 
     })
-    console.log("正在注册请求");
-    res.send(userinfo)
+    
 }
 exports.mylogin=function (req,res) {
     console.log("正在登录请求");
